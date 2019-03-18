142cb7d554cbec9a4aadf4e6f3ebfeebf6e54f42
Mateus Junges <mateusf.junges@gmail.com>
2019-03-18 20:32:03 -0300

    Update config

--- config/acl.php
+++ config/acl.php
@@ -1,21 +1,86 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Models
+    |--------------------------------------------------------------------------
+    |
+    | When using this package, we need to know which Eloquent Model should be used
+    | to retrieve your groups and permissions. Of course, it is just the basics models
+    | needed, but you can use whatever you like.
+    |
+     */
     'models' => [
-        'user' => \MateusJunges\ACL\Http\Models\User::class,
+        /*
+         | The model you want to use as User Model must use MateusJunges\ACL\Traits\UsersTrait
+         */
+        'user'                    => \App\User::class,
+
+        /*
+         | The model you want to use as Permission model must use the MateusJunges\ACL\Traits\PermissionsTrait
+         */
+        'permission'              => \MateusJunges\ACL\Http\Models\Permission::class,
+
+        /*
+         | The model you want to use as Group model must use the MateusJunges\ACL\Traits\GroupsTrait
+         */
+        'group'                   => \MateusJunges\ACL\Http\Models\Group::class,
+
+        /*
+         | The model you want to use as GroupHasPermission model must use the MateusJunges\ACL\Traits\GroupHasPermissionsTrait
+         */
+        'GroupHasPermission'      => \MateusJunges\ACL\Http\Models\GroupHasPermission::class,
+
+        /*
+         | The model you want to use as UserHasPermissions model must use the MateusJunges\ACL\Traits\UserHasPermissionsTrait
+         */
+        'UserHasPermission'       => \MateusJunges\ACL\Http\Models\UserHasPermission::class,
+
+        /*
+         | The model you want to use as UserHasGroup model must use the MateusJunges\ACL\Traits\UserHasGroup
+         */
+        'UserHasGroup'            => \MateusJunges\ACL\Http\Models\UserHasGroup::class,
+
+        /*
+         | The model you want to use as UserHasDeniedPermission model must use the MateusJunges\ACL\Traits\UserHasDeniedPermissionsTrait
+         */
+        'UserHasDeniedPermission' => \MateusJunges\ACL\Http\Models\UserHasDeniedPermission::class
+    ],
+    /*
+    |--------------------------------------------------------------------------
+    | Tables
+    |--------------------------------------------------------------------------
+    | Specify the basics authentication tables that you are using.
+    | Once you required this package, the following tables are
+    | created by default when you run the command
+    |
+    | php artisan migrate
+    |
+    | If you want to change this tables, please keep the basic structure unchanged.
+    |
+     */
+    'tables' => [
+        'groups'                      => 'groups',
+        'permissions'                 => 'permissions',
+        'users'                       => 'users',
+        'group_has_permissions'       => 'group_has_permissions',
+        'user_has_permissions'        => 'user_has_permissions',
+        'user_has_groups'             => 'user_has_groups',
+        'user_has_denied_permissions' => 'user_has_denied_permissions'
     ],
     'app' => [
         'name' => 'ACL'
     ],
 
-        /*
+    /*
     |--------------------------------------------------------------------------
     | Menu Items
     |--------------------------------------------------------------------------
     |
     | Specify your menu items to display in the navbar. Each menu item
-    | should have a text and and a URL. You can also specify an icon from
-    | Font Awesome. A string instead of an array represents a header in sidebar
+    | should have a 'text' and and a 'url' or 'route'.
     | layout. The 'can' is a filter on Laravel's built in Gate functionality.
     |
      */
@@ -26,21 +91,47 @@ return [
             'submenu' => [
                 [
                     'text' => 'Usuários',
-                    'can' => 'users.view',
-                    'route' => 'users.index',
+                    'submenu' => [
+                        [
+                            'text' => 'Lista de usuários',
+                            'can' => 'users.view',
+                            'route' => 'users.index'
+                        ],
+                        [
+                            'text' => 'Usuários removidos',
+                            'can' => 'users.trashed',
+                            'route' => 'users.trashed'
+                        ]
+                    ]
                 ],
                 [
                     'text' => 'Permissões',
                     'can' => 'permissions.view',
                     'route' => 'permissions.index',
+//                    'submenu' => [
+//                        [
+//                            'text' => 'Atribuir permissão a grupo',
+//                            'can' => 'groups.managePermissions',
+//                            'route' => 'groups.'
+//                        ]
+//                    ]
                 ],
                 [
                     'text' => 'Grupos',
-                    'can' => 'groups.view',
-                    'route' => 'groups.index',
-                ],
+                    'can' => 'groups.manage',
+                    'submenu' => [
+                        [
+                            'text' => 'Ver grupos',
+                            'route' => 'groups.index',
+                        ],
+                        [
+                            'text' => 'Grupos removidos',
+                            'route' => 'groups.trashed'
+                        ]
+                    ],
+                ]
             ]
-        ]
+        ],
     ],
 
     /*
