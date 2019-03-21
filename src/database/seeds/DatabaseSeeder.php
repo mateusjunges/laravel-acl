1bab168fee54bbfd7b166fa76b828dd81fad126c
Mateus Junges <mateusf.junges@gmail.com>
2019-03-21 19:26:47 -0300

    Database moved to src and added DatabaseSeeder

---
+++ src/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MateusJunges\database\seeds;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(){
+//        $this->call(Class::class);
+    }
+}
