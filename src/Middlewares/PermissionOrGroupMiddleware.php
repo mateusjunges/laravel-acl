012ac1347d5b7a78b1f974957768a762a2826d6a
Mateus Junges <mateusf.junges@gmail.com>
2019-03-23 18:24:01 -0300

    Added middleware

---
+++ src/Middlewares/PermissionOrGroupMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MateusJunges\ACL\Middlewares;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use MateusJunges\ACL\Exceptions\Unauthorized;
+
+class PermissionOrGroupMiddleware
+{
+    /**
+     * Handle an incoming request
+     *
+     * @param $request
+     * @param Closure $next
+     * @param $groupOrPermissions
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $groupOrPermissions)
+    {
+        if (Auth::guest())
+            throw Unauthorized::notLoggedIn();
+        $permissions = is_array($groupOrPermissions)
+            ? $groupOrPermissions
+            : explode('|', $groupOrPermissions);
+        if (!Auth::user()->hasAnyPermission($permissions)
+            && !Auth::user()->hasAnyGroup($permissions))
+            throw Unauthorized::forGroupsOrPermissions();
+        return $next($request);
+    }
+}
