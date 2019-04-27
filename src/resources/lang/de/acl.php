aa5cc5afe9e99647b2cc82d70138d1061fa092b0
Mateus Junges <contato@mateusjunges.com>
2019-04-27 20:22:58 -0300

    Added german language

---
+++ src/resources/lang/de/acl.php
@@ -0,0 +1,8 @@
+<?php
+
+    return [
+        'forGroups'             => "Dieser Benutzer verfügt nicht über die erforderlichen Gruppen, um auf diese Route zuzugreifen.",
+        'forPermissions'        => "Dieser Benutzer verfügt nicht über die erforderlichen Berechtigungen, um auf diese Route zuzugreifen",
+        'notLoggedIn'            => "Sie sind nicht angemeldet",
+        'forGroupsOrPermissions' => "Sie haben keine Berechtigungen, um auf diese Route zuzugreifen",
+    ];
