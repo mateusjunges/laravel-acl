df5393e1f147d6f657c1664b8a22e4a5b7ae5b11
Mateus Junges <contato@mateusjunges.com>
2019-04-27 20:28:24 -0300

    Added italian language (#26)

---
+++ src/resources/lang/it/acl.php
@@ -0,0 +1,8 @@
+<?php
+
+    return [
+        'forGroups'             => "Questo utente non ha i gruppi necessari per accedere a questa rotta.",
+        'forPermissions'        => "Questo utente non ha le autorizzazioni necessarie per accedere a questa rotta",
+        'notLoggedIn'            => "Non sei loggato.",
+        'forGroupsOrPermissions' => "Non hai nessuna delle autorizzazioni richieste per accedere a questa rotta",
+    ];
