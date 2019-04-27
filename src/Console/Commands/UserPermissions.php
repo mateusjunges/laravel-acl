429b81a5781a41620c5b1db786cc8f32b505c007
Mateus Junges <contato@mateusjunges.com>
2019-04-27 19:30:38 -0300

    FIX #23, update README

--- src/Console/Commands/UserPermissions.php
+++ src/Console/Commands/UserPermissions.php
@@ -4,6 +4,7 @@ namespace Junges\ACL\Console\Commands;
 
 use App\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Schema;
 
 class UserPermissions extends Command
 {
@@ -40,16 +41,32 @@ class UserPermissions extends Command
     {
         try{
             $userParameter = $this->argument('user');
-            $databaseOperator = env('DB_CONNECTION') === 'pgsql' ? 'ilike' : 'LIKE';
-
             if (is_numeric($userParameter)) $user = User::find((int)$userParameter);
-            else if(is_string($userParameter)) $user = User::where('name', $databaseOperator, $userParameter)->first();
+            else if(is_string($userParameter)) {
+                $table = config('acl.tables.users');
+                $columns = $this->verifyColumns($table);
 
+
+                $columns = collect($columns)->map(function ($item){
+                    if ($item['isset_column'])
+                        return $item['column'];
+                })->toArray();
+                $columns = array_unique($columns);
+                $columns = array_filter($columns, 'strlen');
+
+                $userModel = app(config('acl.models.user'));
+
+                $user = $userModel->where(function ($query) use ($userParameter, $columns){
+                    foreach ($columns as $column) {
+                        $query->orWhere($column, $userParameter);
+                   }
+                });
+                $user = $user->first();
+            }
             if (is_null($user)){
                 $this->error('User not found');
                 return;
             }
-
             $permissions = $user->permissions->map(function ($permission){
                return [
                    'name'        => $permission->name,
@@ -57,18 +74,37 @@ class UserPermissions extends Command
                    'description' => $permission->description,
                ];
             });
-
             $this->info('Displaying '.$user->name.'\'s permissions:');
             if ($permissions->count()  == 0){
                 $this->alert('No permissions found');
                 return;
             }
-
             $headers = ['Name', 'Slug', 'Description'];
             $this->table($headers, $permissions);
-
         }catch (\Exception $exception){
             $this->error('Something went wrong');
         }
+    }
+
+    /**
+     * @param $table
+     * @return array
+     */
+    private function verifyColumns($table)
+    {
+        return [
+            [
+                'column' => 'username',
+                'isset_column' => Schema::hasColumn($table, 'username')
+            ],
+            [
+                'column' => 'name',
+                'isset_column' => Schema::hasColumn($table, 'name')
+            ],
+            [
+                'column' => 'email',
+                'isset_column' => Schema::hasColumn($table, 'email')
+            ],
+        ];
     }
 }
