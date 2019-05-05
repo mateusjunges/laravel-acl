373040db37a96721c15bae4056b0d6130dff75d6
Mateus Junges <contato@mateusjunges.com>
2019-05-04 23:58:00 -0300

    Permission::create() throws exception if the permission already exists

--- src/ACLEventsServiceProvider.php
+++ src/ACLEventsServiceProvider.php
@@ -5,13 +5,18 @@ namespace Junges\ACL;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Junges\ACL\Events\GroupSaving;
+use Junges\ACL\Events\PermissionSaving;
 use Junges\ACL\Listeners\GroupSavingListener;
+use Junges\ACL\Listeners\PermissionSavingListener;
 
 class ACLEventsServiceProvider extends ServiceProvider
 {
     public $listen = [
         GroupSaving::class => [
             GroupSavingListener::class
+        ],
+        PermissionSaving::class => [
+            PermissionSavingListener::class
         ]
     ];
 
---
+++ src/Listeners/PermissionSavingListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Junges\ACL\Listeners;
+
+use Junges\ACL\Events\PermissionSaving;
+use Junges\ACL\Exceptions\PermissionAlreadyExistsException;
+
+class PermissionSavingListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  PermissionSaving  $event
+     * @return void
+     */
+    public function handle(PermissionSaving $event)
+    {
+        $permission = $event->permission;
+        $permissionModel = app(config('acl.models.permission'));
+        $permissionAlreadyExists = $permissionModel
+            ->where('slug', $permission->slug)
+            ->orWhere('name', $permission->name)
+            ->first();
+        if (!is_null($permissionAlreadyExists))
+            throw PermissionAlreadyExistsException::create();
+    }
+}
