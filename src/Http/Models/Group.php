aa58b972373b8587a1d72bc7431ff05e21672cef
Mateus Junges <contato@mateusjunges.com>
2019-05-04 22:39:54 -0300

    Throw exception when group already exist

--- src/Http/Models/Group.php
+++ src/Http/Models/Group.php
@@ -3,6 +3,7 @@
 namespace Junges\ACL\Http\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Junges\ACL\Exceptions\GroupAlreadyExistsException;
 use Junges\ACL\Traits\GroupsTrait;
 
 class Group extends Model
@@ -18,4 +19,12 @@ class Group extends Model
     protected $fillable = [
         'name', 'slug', 'description',
     ];
+
+    public static function create(array $attributes = [])
+    {
+        if(static::where('slug', $attributes['slug'])->orWhere('name', $attributes['name'])->first()){
+            throw GroupAlreadyExistsException::create();
+        }
+        return parent::create($attributes);
+    }
 }
