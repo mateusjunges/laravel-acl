baca734d703072d66f5aff5b1850bc9a25a96bb8
Mateus Junges <contato@mateusjunges.com>
2019-05-10 17:26:41 -0300

    Added UserDoesNotExistException (#59)

---
+++ src/Exceptions/UserDoesNotExistException.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Junges\ACL\Exceptions;
+
+use InvalidArgumentException;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserDoesNotExistException extends InvalidArgumentException
+{
+    /**
+     * @param $name
+     * @return UserDoesNotExistException
+     */
+    public static function named($name) : self
+    {
+        $message = trans("acl::acl.user_does_not_exist_with_name");
+        return new static($message." ".$name, Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * @param $id
+     * @return UserDoesNotExistException
+     */
+    public static function withId($id) : self
+    {
+        $message = trans("acl::acl.user_does_not_exist");
+        return new static($message." ".$id, Response::HTTP_BAD_REQUEST);
+    }
+}
