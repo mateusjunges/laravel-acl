671157c609d913e9572e01d31e3f4e7f9738e84d
Mateus Junges <contato@mateusjunges.com>
2019-07-04 00:41:21 -0300

    Add tests for wildcard permissions (#77)

--- tests/WildcardPermissionsTest.php
+++ tests/WildcardPermissionsTest.php
@@ -19,12 +19,45 @@ class WildcardPermissionsTest extends TestCase
     {
         Auth::login($this->testUser);
         $this->testUser->assignPermissions([6, 7]);
-        $this->assertCount(2, $this->testUser->permissions);
+        $this->assertCount(2, Auth::user()->permissions);
         $this->assertTrue(
             Auth::user()->hasPermissionWithWildcards('admin.*.users')
         );
         $this->assertTrue(
             Auth::user()->haspermissionWithWildcards('admin.*')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_deny_access_if_user_does_not_have_matching_permissions()
+    {
+        Auth::login($this->testUser2);
+        $this->assertCount(0, Auth::user()->permissions);
+        $this->assertFalse(
+            Auth::user()->hasPermissionWithWildcards('admin.*')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function checks_with_star_only_always_give_permission_if_the_user_has_at_least_one_permission()
+    {
+        Auth::login($this->testUser);
+        Auth::user()->assignPermissions([1]);
+        $this->assertTrue(Auth::user()->hasPermissionWithWildcards('*'));
+    }
+
+    /**
+     * @test
+     */
+    public function checks_with_star_only_deny_permission_if_the_user_does_not_have__any_permission()
+    {
+        Auth::login($this->testUser);
+        $this->assertFalse(
+            Auth::user()->hasPermissionWithWildcards('*')
         );
     }
 }
