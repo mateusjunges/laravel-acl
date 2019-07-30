321f7c28d30f920699863f236195181cf94f4e08
Mateus Junges <contato@mateusjunges.com>
2019-07-30 17:26:15 -0300

    Added assignPermissions method test

--- tests/Traits/UserPermissionsTest.php
+++ tests/Traits/UserPermissionsTest.php
@@ -12,33 +12,10 @@ class UserPermissionsTest extends TestCase
         parent::setUp();
     }
 
-    public function test_if_a_permission_can_be_assigned_to_a_user()
-    {
-        $this->assertInstanceOf(User::class, $this->testUser->assignPermissions($this->testUserPermission));
-    }
-
     public function test_if_a_permission_can_be_revoked_from_user()
     {
         $this->testUser->assignPermissions($this->testUserPermission);
         $this->assertInstanceOf(User::class, $this->testUser->revokePermissions($this->testUserPermission));
-    }
-
-    public function test_if_a_permission_can_be_assigned_to_user_with_permission_ids()
-    {
-        $this->assertInstanceOf(User::class, $this->testUser->assignPermissions(
-            $this->testUserPermission->id,
-            $this->testUserPermission2->id,
-            $this->testUserPermission3->id
-        ));
-    }
-
-    public function test_if_a_permission_can_be_assigned_to_user_with_mixed_parameters()
-    {
-        $this->assertInstanceOf(User::class, $this->testUser->assignPermissions(
-            $this->testUserPermission->id,
-            $this->testUserPermission2->slug,
-            $this->testUserPermission3
-        ));
     }
 
     public function test_if_a_permission_can_be_revoked_from_user_with_mixed_parameters()
