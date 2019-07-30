8aa96ba94b415b78ad818939e4d5089844681059
Mateus Junges <contato@mateusjunges.com>
2019-07-30 17:30:02 -0300

    Added tests to revokePermissions method

--- tests/Traits/UserPermissionsTest.php
+++ tests/Traits/UserPermissionsTest.php
@@ -12,30 +12,6 @@ class UserPermissionsTest extends TestCase
         parent::setUp();
     }
 
-    public function test_if_a_permission_can_be_revoked_from_user()
-    {
-        $this->testUser->assignPermissions($this->testUserPermission);
-        $this->assertInstanceOf(User::class, $this->testUser->revokePermissions($this->testUserPermission));
-    }
-
-    public function test_if_a_permission_can_be_revoked_from_user_with_mixed_parameters()
-    {
-        $this->assertInstanceOf(User::class, $this->testUser->revokePermissions(
-            $this->testUserPermission->id,
-            $this->testUserPermission2->slug,
-            $this->testUserPermission3
-        ));
-    }
-
-    public function test_if_a_permission_can_be_revoked_from_user_with_permission_ids()
-    {
-        $this->assertInstanceOf(User::class, $this->testUser->revokePermissions(
-            $this->testUserPermission->id,
-            $this->testUserPermission2->id,
-            $this->testUserPermission3->id
-        ));
-    }
-
     public function test_if_it_can_revoke_all_the_user_permissions()
     {
         $this->assertInstanceOf(User::class, $this->testUser->revokeAllPermissions());
