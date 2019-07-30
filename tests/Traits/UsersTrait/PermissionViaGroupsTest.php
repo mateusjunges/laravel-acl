e84cc1954cfce9d25b5cd1929b697913709407e6
Mateus Junges <contato@mateusjunges.com>
2019-07-30 15:41:35 -0300

    Added more tests

--- tests/Traits/UsersTrait/PermissionViaGroupsTest.php
+++ tests/Traits/UsersTrait/PermissionViaGroupsTest.php
@@ -18,4 +18,16 @@ class PermissionViaGroupsTest extends TestCase
         $this->assertTrue($this->testUser->hasGroup($this->testUserGroup));
         $this->assertCount(3, $this->testUser->permissionViaGroups());
     }
+
+    public function test_if_it_returns_only_permission_assigned_via_groups()
+    {
+        $this->testUserGroup->assignPermissions(1, 2, 3);
+        $this->testUser->assignGroup($this->testUserGroup);
+        $this->testUser->assignPermissions(4, 5, 6);
+        $this->assertTrue($this->testUser->hasGroup($this->testUserGroup));
+        $this->assertTrue($this->testUser->hasPermission(4));
+        $this->assertTrue($this->testUser->hasPermission(5));
+        $this->assertTrue($this->testUser->hasPermission(6));
+        $this->assertCount(3, $this->testUser->permissionViaGroups());
+    }
 }
