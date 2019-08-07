4b1d790d70c1743b5fbe4f06c18e7ac3c5cd7a49
Mateus Junges <contato@mateusjunges.com>
2019-08-07 02:29:38 -0300

    Fix users trait

--- src/Traits/UsersTrait.php
+++ src/Traits/UsersTrait.php
@@ -267,7 +267,6 @@ trait UsersTrait
     private function convertToPermissionIds($permissions)
     {
         $model = app(config('acl.models.permission'));
-        $permissions = is_array($permissions[0]) ? $permissions[0] : $permissions;
         $permissions = ! is_array($permissions) ? [$permissions] : $permissions;
 
         return collect(array_map(function ($permission) use ($model) {
@@ -318,6 +317,7 @@ trait UsersTrait
      */
     public function syncPermissions(...$permissions)
     {
+        $permissions = is_array($permissions[0]) ? $permissions[0] : $permissions;
         $permissions = $this->convertToPermissionIds($permissions);
         if ($permissions->count() == 0) {
             return false;
