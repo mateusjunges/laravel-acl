12f2ac70f1c3eef06791454b2433ba53544867b9
Mateus Junges <mateus@junges.dev>
2020-09-16 14:31:09 -0300

    Fix user model namespace for laravel v8.x

--- config/acl.php
+++ config/acl.php
@@ -16,7 +16,7 @@ return [
         /*
          | The model you want to use as User Model must use MateusJunges\ACL\Traits\UsersTrait
          */
-        'user'       => \App\User::class,
+        'user'       => \App\Models\User::class,
 
         /*
          | The model you want to use as Permission model must use the MateusJunges\ACL\Traits\PermissionsTrait
