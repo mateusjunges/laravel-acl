0ae17df79289b08cf2286836fc836b051cea38b3
mateusjunges <mateus@junges.dev>
2021-07-17 19:39:48 -0300

    wip

--- src/Console/Commands/CreateGroup.php
+++ src/Console/Commands/CreateGroup.php
@@ -7,35 +7,16 @@ use Junges\ACL\Exceptions\GroupAlreadyExistsException;
 
 class CreateGroup extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'group:create {name} {slug} {description}';
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+
     protected $description = 'Create a new group on groups table';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
+    public function handle(): int
     {
         $groupModel = app(config('acl.models.group'));
 
