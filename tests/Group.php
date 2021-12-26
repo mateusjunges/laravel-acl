da91274f622d95647fa8596c866e46eee1325068
mateusjunges <mateusjunges@users.noreply.github.com>
2021-12-26 16:08:35 +0000

    Fix styling

--- tests/Group.php
+++ tests/Group.php
@@ -2,13 +2,10 @@
 
 namespace Junges\ACL\Tests;
 
-use Illuminate\Support\Str;
-use Junges\ACL\Events\GroupSaving;
-
 class Group extends \Junges\ACL\Models\Group
 {
     protected $visible = [
         'id',
-        'name'
+        'name',
     ];
 }
--- tests/Permission.php
+++ tests/Permission.php
@@ -2,15 +2,10 @@
 
 namespace Junges\ACL\Tests;
 
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Junges\ACL\Concerns\HasGroups;
-use Junges\ACL\Contracts\Permission as PermissionContract;
-use Junges\ACL\Events\PermissionSaving;
-
 class Permission extends \Junges\ACL\Models\Permission
 {
     protected $visible = [
         'id',
-        'name'
+        'name',
     ];
 }
