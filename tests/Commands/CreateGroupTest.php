840194ed22d50bf7ac9a938f6b77e92e0bac83e4
mateusjunges <mateus@junges.dev>
2022-04-12 10:42:39 -0300

    add command tests

---
+++ tests/Commands/CreateGroupTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Junges\ACL\Tests\Commands;
+
+use Junges\ACL\Contracts\Group;
+use Junges\ACL\Tests\TestCase;
+
+class CreateGroupTest extends TestCase
+{
+    public function testItCanCreateAGroup()
+    {
+        $this->artisan('group:create', [
+            'name' => 'new-group'
+        ]);
+
+        $this->assertCount(1, app(Group::class)->where('name', 'new-group')->get());
+        $this->assertCount(0, app(Group::class)->where('name', 'new-group')->first()->permissions);
+    }
+
+    public function testItCanCreateAGroupWithSpecificGuard()
+    {
+        $this->artisan('group:create', [
+            'name' => 'new-group',
+            'guard' => 'api'
+        ]);
+
+        $this->assertCount(1, app(Group::class)->where('name', 'new-group')
+            ->where('guard_name', 'api')
+            ->get());
+    }
+
+    public function testItCanCreateAGroupWithoutDuplication()
+    {
+        $this->artisan('group:create', ['name' => 'new-group']);
+        $this->artisan('group:create', ['name' => 'new-group']);
+
+        $this->assertCount(1, app(Group::class)->where('name', 'new-group')->get());
+        $this->assertCount(0, app(Group::class)->where('name', 'new-group')->first()->permissions);
+    }
+
+    public function testItCanCreateAGroupAndPermissionAtSameTime()
+    {
+        $this->artisan('group:create', [
+            'name' => 'new-group',
+            'permissions' => 'first permission | second permission',
+        ]);
+
+        $group = app(Group::class)->where('name', 'new-group')->first();
+
+        $this->assertTrue($group->hasPermission('first permission'));
+        $this->assertTrue($group->hasPermission('second permission'));
+    }
+}
